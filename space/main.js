becc7b1bb0d8c1fad625288cfd91d9c537d9e70a
James Ngandu <jamesmngandu@gmail.com>
2023-11-14 12:01:55 +0300

    humberger menu

---
+++ space/main.js
@@ -0,0 +1,8 @@
+const closeBtn = document.getElementById('close-btn');
+const openBtn = document.getElementById('open-btn');
+openBtn.addEventListener('click', function () {
+	alert('Hi mum');
+});
+closeBtn.addEventListener('click', function () {
+	alert('Hello');
+});
