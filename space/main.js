cb7a8b65a7ccd1593da7d87bd14a6e83487ad278
James Ngandu <jamesmngandu@gmail.com>
2023-11-14 12:44:14 +0300

    humberger menu

--- space/main.js
+++ space/main.js
@@ -1,7 +1,7 @@
 const closeBtn = document.getElementById('close-btn');
 const openBtn = document.getElementById('open-btn');
 const navsBtn = document.querySelector('.navs');
-openBtn.addEventListener('click', function () {
+openBtn.addEventListener('click',  () {
 	navsBtn.style.display = 'flex';
 	closeBtn.style.display = 'block';
 	openBtn.style.display = 'none';
